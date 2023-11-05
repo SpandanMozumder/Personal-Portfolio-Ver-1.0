98c2d80b92471084c9a68364d74f47736439d191
Spandan-Mozumder <spandan.mozumder.professional@gmail.com>
2023-11-06 01:10:35 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,76 @@
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+    path: "/",
+  },
+  {
+    id: "about",
+    title: "About",
+    path: "/about",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+    path: "/projects",
+  },
+  {
+    id: "contact",
+    title: "Contact Me",
+    path: "/contact",
+  },
+];
+
+export const projects = [
+  {
+    id: "project1",
+    name: "AI-Summarizer",
+    github: "https://github.com/SpandanMozumder/AI-Summarizer",
+    netlify: "",
+    description:
+      "A website that takes in any URL from the user, and returns a summarized version of the provided URL. The user can also access his history of upto 5 URLs stored in the local storage. The frontend part of the website is made using ReactJS on the development environment of Vite. Tailwind CSS is being used for styling part and an public API is being used for the functionality of summarization from Rapid API Hub.",
+  },
+  {
+    id: "project2",
+    name: "Hoobank",
+    github: "https://github.com/SpandanMozumder/Hoobank",
+    netlify: "",
+    description:
+      "The frontend of the landing page for a bank. The website is made according to the already made Figma File from the Community. The link to the file is provided. https://www.figma.com/community/file/1227560835659149296",
+  },
+  {
+    id: "project3",
+    name: "Telephone Directory",
+    github: "https://github.com/SpandanMozumder/Contact-Manager-Backend",
+    netlify: "",
+    description:
+      "The backend part of the telephone directory application using REST APIs. The application has user authentication as well as api routes for creating, editing, and deleting contacts.",
+  },
+  {
+    id: "project4",
+    name: "Book Store",
+    github: "https://github.com/SpandanMozumder/Book-Store",
+    netlify: "",
+    description:
+      "A MERN Stack website with both the frontend and the backend part integrated. The user can create, edit, and delete books.",
+  },
+  {
+    id: "project5",
+    name: "Food",
+    github: "https://github.com/SpandanMozumder/GOFood",
+    netlify: "",
+    description:
+      "A fully functional MERN Stack website using REST APIs with authentication, add to cart, checkout, past orders functionalities",
+  },
+];
+
+export const currentprojects = [
+  {
+    id: "project1",
+    name: "SwasthaVrikshya",
+    github: "https://github.com/SpandanMozumder/Smart-India-Hackathon-2023",
+    netlify: "",
+    description:
+      "The solution prototype for a problem statement in the 2023 Smart India Hackathon",
+  },
+];
